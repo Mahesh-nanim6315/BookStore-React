4d3a140060c7ece3ee8ea397b3e855773e278f67
Mahesh-nanim6315 <maheshn7780@gmail.com>
2026-04-07 20:15:18 +0530

    fixed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware('maintenance')->group(function () {
-    require __DIR__.'/api/public.php';
+    require_once __DIR__.'/api/public.php';
 });
-
 
 Route::prefix('v1')->middleware(['auth:sanctum', 'maintenance'])->group(function () {
-    require __DIR__.'/api/protected.php';
+    require_once __DIR__.'/api/protected.php';
 });
 
-require __DIR__.'/api/admin.php';
+require_once __DIR__.'/api/admin.php';
