233895e9482f0e9cd4d00d64b2cec5c958e5e3d8
Mahesh-nanim6315 <maheshn7780@gmail.com>
2026-04-08 11:52:57 +0530

    okk

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,5 +9,4 @@ Route::prefix('v1')->middleware('maintenance')->group(function () {
 Route::prefix('v1')->middleware(['auth:sanctum', 'maintenance'])->group(function () {
     require_once __DIR__.'/api/protected.php';
 });
-
-require_once __DIR__.'/api/admin.php';
+require_once __DIR__.'/api/admin.php'; 
